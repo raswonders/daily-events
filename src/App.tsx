17d270bf477ac5c17319029912476d1b0f76dce9
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-06-03 13:03:17 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Table } from "./components/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App_title">Daily Events</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,29 @@
+import "./Table.css";
+
+const rows = Array.from({ length: 24 }, (_, index) => {
+  return {
+    time: index * 60,
+    events: [],
+  };
+});
+
+function getTime(minutes: number) {
+  const hours = Math.floor(minutes / 60);
+  const minutesRemaining = minutes % 60;
+  return `${hours}:${minutesRemaining.toString().padStart(2, "0")}`;
+}
+
+export function Table() {
+  return (
+      <div className="Table">
+        {rows.map((row) => (
+          <div key={row.time} className="Table_row">
+            <div className="Table_header">{getTime(row.time)}</div>
+            <div className="Table_cell">
+              {row.events.length === 0 ? "No events" : row.events}
+            </div>
+          </div>
+        ))}
+      </div>
+  )
+}
