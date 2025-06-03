a38bc52d91fd5837968dd213868b9019e4fff9c5
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-06-03 20:18:33 +0200

    refine table styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,7 +10,9 @@ const rows = Array.from({ length: 24 }, (_, index) => {
 function getTime(minutes: number) {
   const hours = Math.floor(minutes / 60);
   const minutesRemaining = minutes % 60;
-  return `${hours}:${minutesRemaining.toString().padStart(2, "0")}`;
+  return `${hours.toString().padStart(2, "0")}:${minutesRemaining
+    .toString()
+    .padStart(2, "0")}`;
 }
 
 export function Table() {
