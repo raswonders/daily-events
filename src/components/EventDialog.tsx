c54440967077c29b4b0905cad72168543fa3c99f
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-06-06 11:48:01 +0200

    enhance EventDialog with form functionality and improved styling

--- src/components/EventDialog.tsx
+++ src/components/EventDialog.tsx
@@ -1,6 +1,7 @@
-import { Dialog } from "radix-ui";
+import { Dialog, Form } from "radix-ui";
 import "./EventDialog.css";
 import { X } from "lucide-react";
+import type { FormEvent } from "react";
 
 interface Props {
   open: boolean;
@@ -8,16 +9,39 @@ interface Props {
 }
 
 export function EventDialog({ open, onOpenChange }: Props) {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form submitted");
+    console.log(event);
+    onOpenChange(false);
+  };
+
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
         <Dialog.Overlay className="Dialog_overlay" />
         <Dialog.Content className="Dialog_content">
-          <Dialog.Close className="Dialog_close">
-            <X />
-          </Dialog.Close>
-          <Dialog.Title>Add title</Dialog.Title>
-          <Dialog.Description>Add description</Dialog.Description>
+          <div className="Dialog_header">
+            <Dialog.Title className="Dialog_title">New Event</Dialog.Title>
+            <Dialog.Close className="Dialog_close">
+              <X />
+            </Dialog.Close>
+          </div>
+          <Form.Root className="Form_root" onSubmit={handleSubmit}>
+            <Form.Field name="title" className="Form_field">
+              <Form.Control placeholder="Add title" className="Form_control" />
+            </Form.Field>
+            <Form.Field name="description" className="Form_field">
+              <Form.Control
+                placeholder="Add description"
+                className="Form_control"
+                asChild
+              >
+                <textarea />
+              </Form.Control>
+            </Form.Field>
+            <Form.Submit>Save</Form.Submit>
+          </Form.Root>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
